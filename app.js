ebcf8bb604cef796d321bfc8b592b1cd07fc76e2
ianpaul0520 <ianpaul0520@gmail.com>
2020-08-21 22:10:42 +0800

    worked on adding a color on an object

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let palleteText = document.getElementById("pallete-text");
 let backgroundDiv = document.getElementById("background-div");
-let textNode = backgroundDiv.style.background;
+let addButton = document.getElementById("add-button");
+// let textNode = backgroundDiv.style.background;
 
+let palleteContainer = document.getElementById("pallete-container");
 let colors = ["cffffe", "f9f7d9", "fce2ce", "ffc1f3"];
 
 // let x = setInterval(() => {
@@ -18,11 +20,47 @@ function setBackgroundColor(element, color) {
 
 let collection = {};
 let counter = 0;
-colors.forEach((elem) => {
-  let propName = "color" + counter;
-  collection[propName] = elem;
-  counter++;
-});
 
-collection.color1 = "#fff";
-console.log(collection);
+// colors.forEach((elem) => {
+//   let propName = "color" + counter;
+//   collection[propName] = elem;
+//   counter++;
+// });
+
+function createSelection(color) {
+  let p = document.createElement("p");
+
+  let neededClass = ["subtitle", "has-text-centered", "px-2", "py-2"];
+  neededClass.forEach((ele) => {
+    p.classList.add(ele);
+  });
+
+  p.addEventListener("click", function () {
+    backgroundDiv.style.background = color;
+  });
+
+  p.innerHTML = color;
+  return p;
+}
+
+function addToCollection(color) {
+  let propName = "color" + counter;
+  collection[propName] = color;
+  counter++;
+  console.log(propName);
+}
+
+function loadUpItems() {
+  for (let item in collection) {
+    if (collection.hasOwnProperty(item)) {
+      console.log(collection[item]);
+    } else {
+      console.error("Failed to retrieved the Rest.");
+      return;
+    }
+  }
+}
+addButton.addEventListener("click", function () {
+  addToCollection("green");
+  loadUpItems();
+});
