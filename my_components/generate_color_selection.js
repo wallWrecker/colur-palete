e70a140312810e46d877f76289e67b022ff5423c
wallWrecker <sinantakboy@gmail.com>
2020-09-05 10:13:11 +0800

    adding mouseover  & mouseout event listener

--- my_components/generate_color_selection.js
+++ my_components/generate_color_selection.js
@@ -2,15 +2,26 @@ let column_container = document.getElementsByClassName("columns")[0];
 
 const createChildOfColumn = (text, color) => {
   let myColumn = document.createElement("DIV");
-  let classesArray = ["column", "is-1", "px-1", "py-1", "color-house"];
+  let classesArray = [
+    "column",
+    "color_house",
+    "is-1",
+    "px-3",
+    "py-3",
+    "mx-1",
+    "my-1",
+    "has-text-centered",
+  ];
+  //append all needed class to the newly created element.
   classesArray.forEach((ele) => myColumn.classList.add(ele));
   myColumn.textContent = text;
-  myColumn.style.backgroundColor = color;
+  myColumn.addEventListener("mouseover", function () {
+    myColumn.style.backgroundColor = "#" + color;
+  });
+  myColumn.addEventListener("mouseout", function () {
+    myColumn.style.backgroundColor = "";
+  });
   return myColumn;
 };
 
-const sayHi = (your_name) => {
-  alert(`Hi there! ${your_name}`);
-};
-
-export { column_container, createChildOfColumn, sayHi };
+export { column_container, createChildOfColumn };
